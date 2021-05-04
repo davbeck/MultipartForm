f1fe739f7f1403bca16cc04edad5f88596f46c32
Arnold Plakolli <plakolliarnold@gmail.com>
2021-05-04 18:51:24 +0200

    Added an Xcode project so that Carthage can build it.

--- Package.swift
+++ Package.swift
@@ -3,27 +3,19 @@ import PackageDescription
 
 let package = Package(
     name: "MultipartForm",
+    platforms: [
+		.iOS(.v10),
+	],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MultipartForm",
             targets: ["MultipartForm"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MultipartForm",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "MultipartFormTests",
-            dependencies: ["MultipartForm"]
-        ),
+            path: "MultipartForm"
+        )
     ]
 )
